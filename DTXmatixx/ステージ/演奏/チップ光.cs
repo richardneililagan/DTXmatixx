2cfab8bebc7bc5ae71da214d04360bb7f002720a
くまかみ工房 <kumakamikoubou@gmail.com>
2017-09-23 14:23:35 +0900

    チップ光の輪を実装。

--- DTXmatixx/ステージ/演奏/チップ光.cs
+++ DTXmatixx/ステージ/演奏/チップ光.cs
@@ -14,6 +14,7 @@ namespace DTXmatixx.ステージ.演奏
 		public チップ光()
 		{
 			this.子リスト.Add( this._放射光 = new 画像( @"$(System)images\チップ光.png" ) { 加算合成 = true } );
+			this.子リスト.Add( this._光輪 = new 画像( @"$(System)images\チップ光輪.png" ) { 加算合成 = true } );
 		}
 
 		protected override void On活性化( グラフィックデバイス gd )
@@ -68,22 +69,61 @@ namespace DTXmatixx.ステージ.演奏
 							status.アニメ用メンバを解放する();
 
 							// 初期状態
-							status.回転角 = new Variable( gd.Animation.Manager, initialValue: 0.0 );
-							status.拡大率 = new Variable( gd.Animation.Manager, initialValue: 1.0 );
+							status.放射光の回転角 = new Variable( gd.Animation.Manager, initialValue: 0.0 );
+							status.放射光の拡大率 = new Variable( gd.Animation.Manager, initialValue: 1.0 );
+							status.光輪の拡大率 = new Variable( gd.Animation.Manager, initialValue: 0.0 );
+							status.光輪の不透明度 = new Variable( gd.Animation.Manager, initialValue: 1.0 );
 							status.ストーリーボード = new Storyboard( gd.Animation.Manager );
 
 							double 期間sec;
 
-							// シーン1. 回転しつつ縮小しつつ透明化
-							期間sec = 0.1;
-							using( var 回転角の遷移 = gd.Animation.TrasitionLibrary.Linear( duration: 期間sec, finalValue: 45.0 ) )
-							using( var 拡大率の遷移1 = gd.Animation.TrasitionLibrary.Linear( duration: 期間sec/2.0, finalValue: 1.5 ) )
-							using( var 拡大率の遷移2 = gd.Animation.TrasitionLibrary.Linear( duration: 期間sec/2.0, finalValue: 0.7 ) )
+							#region " (1) 放射光 "
+							//----------------
 							{
-								status.ストーリーボード.AddTransition( status.回転角, 回転角の遷移 );
-								status.ストーリーボード.AddTransition( status.拡大率, 拡大率の遷移1 );
-								status.ストーリーボード.AddTransition( status.拡大率, 拡大率の遷移2 );
+								// シーン1. 回転しつつ拡大縮小
+								期間sec = 0.1;
+								using( var 回転角の遷移 = gd.Animation.TrasitionLibrary.Linear( duration: 期間sec, finalValue: 45.0 ) )
+								using( var 拡大率の遷移1 = gd.Animation.TrasitionLibrary.Linear( duration: 期間sec / 2.0, finalValue: 1.5 ) )
+								using( var 拡大率の遷移2 = gd.Animation.TrasitionLibrary.Linear( duration: 期間sec / 2.0, finalValue: 0.7 ) )
+								{
+									status.ストーリーボード.AddTransition( status.放射光の回転角, 回転角の遷移 );
+									status.ストーリーボード.AddTransition( status.放射光の拡大率, 拡大率の遷移1 );
+									status.ストーリーボード.AddTransition( status.放射光の拡大率, 拡大率の遷移2 );
+								}
+
+								// シーン2. 縮小して消滅
+								期間sec = 0.1;
+								using( var 拡大率の遷移 = gd.Animation.TrasitionLibrary.Linear( duration: 期間sec / 2.0, finalValue: 0.0 ) )
+								{
+									status.ストーリーボード.AddTransition( status.放射光の拡大率, 拡大率の遷移 );
+								}
 							}
+							//----------------
+							#endregion
+
+							#region " (2) 光輪 "
+							//----------------
+							{
+								// シーン1. ある程度まで拡大
+								期間sec = 0.05;
+								using( var 光輪の拡大率の遷移 = gd.Animation.TrasitionLibrary.Linear( duration: 期間sec, finalValue: 0.6 ) )
+								using( var 光輪の不透明度の遷移 = gd.Animation.TrasitionLibrary.Constant( duration: 期間sec ) )
+								{
+									status.ストーリーボード.AddTransition( status.光輪の拡大率, 光輪の拡大率の遷移 );
+									status.ストーリーボード.AddTransition( status.光輪の不透明度, 光輪の不透明度の遷移 );
+								}
+
+								// シーン2. ゆっくり拡大しつつ消える
+								期間sec = 0.15;
+								using( var 光輪の拡大率の遷移 = gd.Animation.TrasitionLibrary.Linear( duration: 期間sec, finalValue: 0.8 ) )
+								using( var 光輪の不透明度の遷移 = gd.Animation.TrasitionLibrary.Linear( duration: 期間sec, finalValue: 0.0 ) )
+								{
+									status.ストーリーボード.AddTransition( status.光輪の拡大率, 光輪の拡大率の遷移 );
+									status.ストーリーボード.AddTransition( status.光輪の不透明度, 光輪の不透明度の遷移 );
+								}
+							}
+							//----------------
+							#endregion
 
 							// 開始
 							status.ストーリーボード.Schedule( gd.Animation.Timer.Time );
@@ -97,15 +137,36 @@ namespace DTXmatixx.ステージ.演奏
 						#region " 表示中 "
 						//----------------
 						{
-							var 転送元矩形dpx = (RectangleF) this._放射光の矩形リスト[ レーン.ToString() ].Value;
-							var 転送元矩形の中心dpx = new Vector2( 転送元矩形dpx.Width / 2f, 転送元矩形dpx.Height / 2f );
+							#region " (1) 放射光 "
+							//----------------
+							{
+								var 転送元矩形dpx = (RectangleF) this._放射光の矩形リスト[ レーン.ToString() ].Value;
+								var 転送元矩形の中心dpx = new Vector2( 転送元矩形dpx.Width / 2f, 転送元矩形dpx.Height / 2f );
 
-							var 変換行列2D =
-								Matrix3x2.Scaling( (float)status.拡大率.Value, (float) status.拡大率.Value, center: 転送元矩形の中心dpx ) *
-								Matrix3x2.Rotation( MathUtil.DegreesToRadians( (float) status.回転角.Value ), center: 転送元矩形の中心dpx ) *
-								Matrix3x2.Translation( status.表示中央位置dpx.X - 転送元矩形の中心dpx.X, status.表示中央位置dpx.Y - 転送元矩形の中心dpx.Y );
+								var 変換行列2D =
+									Matrix3x2.Scaling( (float) status.放射光の拡大率.Value, (float) status.放射光の拡大率.Value, center: 転送元矩形の中心dpx ) *
+									Matrix3x2.Rotation( MathUtil.DegreesToRadians( (float) status.放射光の回転角.Value ), center: 転送元矩形の中心dpx ) *
+									Matrix3x2.Translation( status.表示中央位置dpx.X - 転送元矩形の中心dpx.X, status.表示中央位置dpx.Y - 転送元矩形の中心dpx.Y );
 
-							this._放射光.描画する( gd, 変換行列2D, 転送元矩形: 転送元矩形dpx );
+								this._放射光.描画する( gd, 変換行列2D, 転送元矩形: 転送元矩形dpx );
+							}
+							//----------------
+							#endregion
+
+							#region " (2) 光輪 "
+							//----------------
+							{
+								var 転送元矩形dpx = (RectangleF) this._放射光の矩形リスト[ レーン.ToString() ].Value;
+								var 転送元矩形の中心dpx = new Vector2( 転送元矩形dpx.Width / 2f, 転送元矩形dpx.Height / 2f );
+
+								var 変換行列2D =
+									Matrix3x2.Scaling( (float) status.光輪の拡大率.Value, (float) status.光輪の拡大率.Value, center: 転送元矩形の中心dpx ) *
+									Matrix3x2.Translation( status.表示中央位置dpx.X - 転送元矩形の中心dpx.X, status.表示中央位置dpx.Y - 転送元矩形の中心dpx.Y );
+
+								this._光輪.描画する( gd, 変換行列2D, 転送元矩形: 転送元矩形dpx, 不透明度0to1: (float) status.光輪の不透明度.Value );
+							}
+							//----------------
+							#endregion
 						}
 
 						// 全部終わったら非表示へ。
@@ -124,6 +185,7 @@ namespace DTXmatixx.ステージ.演奏
 		}
 
 		private 画像 _放射光 = null;
+		private 画像 _光輪 = null;
 		private 矩形リスト _放射光の矩形リスト = null;
 
 		/// <summary>
@@ -143,8 +205,10 @@ namespace DTXmatixx.ステージ.演奏
 
 			public readonly Vector2 表示中央位置dpx;
 
-			public Variable 回転角 = null;
-			public Variable 拡大率 = null;
+			public Variable 放射光の回転角 = null;
+			public Variable 放射光の拡大率 = null;
+			public Variable 光輪の拡大率 = null;
+			public Variable 光輪の不透明度 = null;
 			public Storyboard ストーリーボード = null;
 
 			public 表示レーンステータス( 表示レーン種別 lane )
@@ -165,8 +229,10 @@ namespace DTXmatixx.ステージ.演奏
 			{
 				this.ストーリーボード?.Abandon();
 				FDKUtilities.解放する( ref this.ストーリーボード );
-				FDKUtilities.解放する( ref this.回転角 );
-				FDKUtilities.解放する( ref this.拡大率 );
+				FDKUtilities.解放する( ref this.放射光の回転角 );
+				FDKUtilities.解放する( ref this.放射光の拡大率 );
+				FDKUtilities.解放する( ref this.光輪の拡大率 );
+				FDKUtilities.解放する( ref this.光輪の不透明度 );
 			}
 		}
 		private Dictionary<表示レーン種別, 表示レーンステータス> _レーンtoステータス = null;
