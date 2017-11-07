f7ae07cffabc2fad2fc640f8e6e5826c39b620d9
ＦＲＯＭ <kumakamikoubou@gmail.com>
2017-11-07 18:51:31 +0900

    Log出力をログファイルにも落とすよう修正。
    ログファイルは、$(AppData)/DTXmatixx/Logs/ フォルダ内に、起動ごとに１つずつ作成する。
    バージョンを002から003へ更新。

--- DTXmatixx/Program.cs
+++ DTXmatixx/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Text;
 using System.Windows.Forms;
 using FDK;
 
@@ -18,6 +21,31 @@ namespace DTXmatixx
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault( false );
 
+			#region " ログファイルへのログの複製出力開始 "
+			//----------------
+			Trace.AutoFlush = true;
+			try
+			{
+				var AppDataフォルダ名 = Path.Combine( Environment.GetFolderPath( Environment.SpecialFolder.ApplicationData, Environment.SpecialFolderOption.Create ), @"DTXMatixx\" );
+				if( !( Directory.Exists( AppDataフォルダ名 ) ) )
+					Directory.CreateDirectory( AppDataフォルダ名 );   // なければ作成。
+
+				var ログフォルダ名 = Path.Combine( AppDataフォルダ名, "Logs" );
+				if( !( Directory.Exists( ログフォルダ名 ) ) )
+					Directory.CreateDirectory( ログフォルダ名 );   // なければ作成。
+
+				var ログファイル名 = Path.Combine( ログフォルダ名, "Log." + DateTime.Now.ToString( "yyyyMMdd.HHmmss" ) + ".txt" );
+
+				Trace.Listeners.Add( new TraceLogListener( new StreamWriter( ログファイル名, false, Encoding.GetEncoding( "utf-8" ) ) ) );
+			}
+			catch( UnauthorizedAccessException )
+			{
+				MessageBox.Show( "Failed to create log file.", "DTXMania boot error", MessageBoxButtons.OK, MessageBoxIcon.Error );
+				Environment.Exit( 1 );
+			}
+			//----------------
+			#endregion
+
 			Log.現在のスレッドに名前をつける( "描画" );
 			Log.Header( "アプリケーションを起動します。" );
 
@@ -27,6 +55,9 @@ namespace DTXmatixx
 				Log.Header( "アプリケーションを終了します。" );
 			}
 			Log.Header( "アプリケーションを終了しました。" );
+
+			Trace.WriteLine( "" );
+			Trace.WriteLine( "遊んでくれてありがとう！" );
 		}
 	}
 }
--- DTXmatixx/Properties/AssemblyInfo.cs
+++ DTXmatixx/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "2.0.0.0" )]
-[assembly: AssemblyFileVersion( "2.0.0.0" )]
+[assembly: AssemblyVersion( "3.0.0.0" )]
+[assembly: AssemblyFileVersion( "3.0.0.0" )]
 
 // 公開先のアセンブリを指定する
 // [assembly: InternalsVisibleTo("公開先のアセンブリ名")]
